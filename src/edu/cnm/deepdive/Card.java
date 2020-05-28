e50b2f8df9fb76f5ad158ab4d2b9f4f332907698
Curtis Chavez <cchavez539@cnm.edu>
2020-05-28 11:04:32 -0600

    Added comments for Oracle website.

--- src/edu/cnm/deepdive/Card.java
+++ src/edu/cnm/deepdive/Card.java
@@ -5,6 +5,12 @@ public class Card {
   private final Suit suit;
   private final Rank rank;
 
+  /**
+   * Create an instance of {@code Card} with the specified {@link Suit} and {@link Rank}.
+   *
+   * @param suit Selected suit
+   * @param rank Selected Rank
+   */
   public Card(Suit suit, Rank rank) {
     this.suit = suit;
     this.rank = rank;
